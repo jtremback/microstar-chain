b1e2c52ab57a6658ff91306652d2332d764cd710
Jehan <jehan.tremback@gmail.com>
2015-03-08 21:24:54 -0700

    Make copy and write use same code, get rid of public 'validate'

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ module.exports = {
   readOne: llibrarian.makeReadOne(read),
   write: write,
   writeOne: llibrarian.makeWriteOne(write),
+  copyOne: llibrarian.makeWriteOne(copy),
   copy: copy,
-  validate: validate,
   createDocs: createDocs,
   sequential: sequential,
   index_defs: [
@@ -30,34 +30,77 @@ module.exports = {
 //   chain_id: String
 // }
 
-// Formats messages and then writes them to db
-function write (settings, callback) {
+function createEnvelopes (settings) {
+  return processMessages(settings, mMessage.createEnvelope, settings.keys.public_key)
+}
+
+function validate (settings) {
+  return processMessages(settings, mMessage.validate)
+}
+
+function processMessages (settings, func, public_key) {
   var previous
-  return pull(
-    pull.asyncMap(function (message, callback) {
-      if (!previous) {
-        // Get previous message from db
-        llibrarian.readOne(settings, {
-          k: ['public_key', 'chain_id', 'sequence'],
-          v: [settings.keys.public_key, message.chain_id],
-          peek: 'last'
-        }, function (err, prev) {
-          if (prev) { prev = prev.value }
-          mMessage.createEnvelope(settings, message, prev, function (err, message) {
-            previous = message
-            return callback(err, message)
-          })
-        })
-      } else {
-        mMessage.createEnvelope(settings, message, previous, function (err, message) {
+  return pull.asyncMap(function (message, callback) {
+    if (!previous) {
+      // Get previous message from db
+      llibrarian.readOne(settings, {
+        k: ['public_key', 'chain_id', 'sequence'],
+        v: [(public_key || message.public_key), message.chain_id],
+        peek: 'last'
+      }, function (err, prev) {
+        // Now that we have the previous message...
+        if (prev) { prev = prev.value }
+        // Create envelope for message and callback
+        func(settings, message, prev, function (err, message) {
+          // Set previous
           previous = message
           return callback(err, message)
         })
-      }
-    }),
+      })
+    } else {
+      // Create envelope for message and callback
+      func(settings, message, previous, function (err, message) {
+        // Set previous
+        previous = message
+        return callback(err, message)
+      })
+    }
+  })
+}
+
+// Formats messages and then writes them to db
+function write (settings, callback) {
+  return pull(
+    createEnvelopes(settings),
     createDocs(settings),
     llibrarian.write(settings, callback)
   )
+}
+
+// Saves messages without making envelopes, but with validation
+// against past messages
+function copy (settings, callback) {
+  return pull(
+    validate(settings),
+    createDocs(settings),
+    llibrarian.write(settings, callback)
+  )
+}
+
+// function validate (settings, initial) {
+//   var previous
+//   return pull.asyncMap(function (message, callback) {
+//     mMessage.validate(settings, message, previous || initial, function (err, message) {
+//       previous = message
+//       callback(err, message)
+//     })
+//   })
+// }
+
+function createDocs (settings) {
+  return pull.asyncMap(function (message, callback) {
+    mMessage.createDoc(settings, message, callback)
+  })
 }
 
 function read (settings, query) {
@@ -88,30 +131,3 @@ function sequential (settings, public_key, chain_id, sequence) {
 
   return read(settings, query)
 }
-
-// Saves messages without making envelopes, but with validation
-// against past messages
-function copy (settings, initial, callback) {
-  return pull(
-    validate(settings, initial),
-    createDocs(settings),
-    llibrarian.write(settings, callback)
-  )
-}
-
-function validate (settings, initial) {
-  var previous
-  return pull.asyncMap(function (message, callback) {
-    mMessage.validate(settings, message, previous || initial, function (err, message) {
-      previous = message
-      callback(err, message)
-    })
-  })
-}
-
-function createDocs (settings) {
-  return pull.asyncMap(function (message, callback) {
-    mMessage.createDoc(settings, message, callback)
-  })
-}
-
--- test/index.js
+++ test/index.js
@@ -7,15 +7,34 @@ var pull = require('pull-stream')
 var pl = require('pull-level')
 var async = require('async')
 var crypto = require('crypto')
+var clone = require('lodash.clone')
+var stringify = require('stable-stringify')
+
+
 
 // var level = require('level-test')()
+
 var level = require('level')
+
 var rimraf = require('rimraf')
 rimraf.sync('./test1.db')
+rimraf.sync('./test2.db')
+
 var db = level('./test1.db', { valueEncoding: 'json' })
+var db2 = level('./test2.db', { valueEncoding: 'json' })
+
+// function dwrite (data) {
+//   console.log(stringify(data))
+// }
+// var dump = require('level-dump')
+// dump(db, dwrite, function (err) {
+//   if (err) { throw err }
+//   console.log('\n\n\n\n\n\n\n\n')
+//   dump(db2, dwrite, function () {})
+// })
+
 
 var trace = require('get-trace')
-
 function tracify (t) {
   function attach (func) {
     return function () {
@@ -92,65 +111,74 @@ function tests (keys) {
         pl.read(db),
         pull.collect(function (err, arr) {
           t.error(err)
-          t.deepEqual(sha1hex(arr), '752a2fe53263889d21c01c4807664d94f32ab3bb')
+          t.deepEqual(sha1hex(stringify(arr)), '83330ec925444693f1d34026cb00ed86b7652223')
           t.end()
         })
       )
     })
   })
+
   var messages = [{
-      chain_id: 'holiday-carols:2014',
-      content: 'Fa',
-      previous: null,
-      public_key: 'N3DyaY1o1EmjPLUkRQRu41/g/xKe/CR/cCmatA78+zY=7XuCMMWN3y/r6DeVk7YGY8j/0rWyKm3TNv3S2cbmXKk=',
-      sequence: 0,
-      signature: '8qq7OacU+0dvBjgjwCR60l4PGekmr9Dd/F8XPmSBCvJdJ4bk9Raa0/AGVJx1r9ABIR4M19Qq7BQbw+aQGHNwAQ==',
-      timestamp: 1418804138168,
-      type: 'holiday-carols:syllable'
-    },
-    {
-      chain_id: 'holiday-carols:2014',
-      content: 'La',
-      previous: 'svQZgkOrqLQb4JEO/WidhH/Doyn5AK6pGHHVWqmRaiFIG9qmtpBP2YDZMTyGfMeSTGxk0WoPz0QNMOaLUCZeqQ==',
-      public_key: 'N3DyaY1o1EmjPLUkRQRu41/g/xKe/CR/cCmatA78+zY=7XuCMMWN3y/r6DeVk7YGY8j/0rWyKm3TNv3S2cbmXKk=',
-      sequence: 1,
-      signature: 'pitLmr//o8tWcsD5qgj87aC024H/PsPeFzwmM9w8hrQdDqSRHnzdBc+KIoQ9Zwrqc4A+M++jo7dhcuUy9Xp4CQ==',
-      timestamp: 1418804138169,
-      type: 'holiday-carols:syllable'
-    },
-    {
-      chain_id: 'holiday-carols:2014',
-      content: 'Laa',
-      previous: 'zcSN5BdLPyZMLwsN0NkOWXjNHbnss2YRRpNhdA63V+mliYjCTj3ySkbLIGcy4Jn9c5oHdQI8eMjdiHgkzZ7e3Q==',
-      public_key: 'N3DyaY1o1EmjPLUkRQRu41/g/xKe/CR/cCmatA78+zY=7XuCMMWN3y/r6DeVk7YGY8j/0rWyKm3TNv3S2cbmXKk=',
-      sequence: 2,
-      signature: 'WHu819414rDA3e4mgHijrcEGa7FEjpaC/FJMuu5fXzcEgqFe655tdUxbNEuZj9Jyon1WhLOJDGOHIJ8wsXY5AA==',
-      timestamp: 1418804138170,
-      type: 'holiday-carols:syllable'
-    }]
+    chain_id: 'holiday-carols:2014',
+    content: 'Fa',
+    previous: null,
+    public_key: 'N3DyaY1o1EmjPLUkRQRu41/g/xKe/CR/cCmatA78+zY=7XuCMMWN3y/r6DeVk7YGY8j/0rWyKm3TNv3S2cbmXKk=',
+    sequence: 0,
+    signature: '8qq7OacU+0dvBjgjwCR60l4PGekmr9Dd/F8XPmSBCvJdJ4bk9Raa0/AGVJx1r9ABIR4M19Qq7BQbw+aQGHNwAQ==',
+    timestamp: 1418804138168,
+    type: 'holiday-carols:syllable'
+  },
+  {
+    chain_id: 'holiday-carols:2014',
+    content: 'La',
+    previous: 'svQZgkOrqLQb4JEO/WidhH/Doyn5AK6pGHHVWqmRaiFIG9qmtpBP2YDZMTyGfMeSTGxk0WoPz0QNMOaLUCZeqQ==',
+    public_key: 'N3DyaY1o1EmjPLUkRQRu41/g/xKe/CR/cCmatA78+zY=7XuCMMWN3y/r6DeVk7YGY8j/0rWyKm3TNv3S2cbmXKk=',
+    sequence: 1,
+    signature: 'pitLmr//o8tWcsD5qgj87aC024H/PsPeFzwmM9w8hrQdDqSRHnzdBc+KIoQ9Zwrqc4A+M++jo7dhcuUy9Xp4CQ==',
+    timestamp: 1418804138169,
+    type: 'holiday-carols:syllable'
+  },
+  {
+    chain_id: 'holiday-carols:2014',
+    content: 'Laa',
+    previous: 'zcSN5BdLPyZMLwsN0NkOWXjNHbnss2YRRpNhdA63V+mliYjCTj3ySkbLIGcy4Jn9c5oHdQI8eMjdiHgkzZ7e3Q==',
+    public_key: 'N3DyaY1o1EmjPLUkRQRu41/g/xKe/CR/cCmatA78+zY=7XuCMMWN3y/r6DeVk7YGY8j/0rWyKm3TNv3S2cbmXKk=',
+    sequence: 2,
+    signature: 'WHu819414rDA3e4mgHijrcEGa7FEjpaC/FJMuu5fXzcEgqFe655tdUxbNEuZj9Jyon1WhLOJDGOHIJ8wsXY5AA==',
+    timestamp: 1418804138170,
+    type: 'holiday-carols:syllable'
+  }]
 
-  test('validate', function (t) {
+  test('copy', function (t) {
     t = tracify(t)
-    t.plan(2)
+    var _settings = clone(settings)
+    _settings.db = db2
 
-
-    pull(
-      pull.values([messages[1], messages[2]]),
-      mChain.validate(settings, messages[0]),
-      pull.collect(function (err, arr) {
-        if (err) { throw err }
-        t.deepEqual(arr, [messages[1], messages[2]], 'Partial chain with supplied initial message.')
-      })
-    )
-
-    pull(
-      pull.values([messages[0], messages[1], messages[2]]),
-      mChain.validate(settings),
-      pull.collect(function (err, arr) {
-        if (err) { throw err }
-        t.deepEqual(arr, [messages[0], messages[1], messages[2]], 'Partial chain without supplied initial message.')
-      })
-    )
+    async.series([
+      // This writes a stream of two messages to the db.
+      function (callback) {
+        pull(
+          pull.values([messages[0], messages[1]]),
+          mChain.copy(_settings, callback)
+        )
+      },
+      // This writes another, after the first two are done.
+      function (callback) {
+        setTimeout(function () {
+          mChain.copyOne(_settings, messages[2], callback)
+        }, 1000)
+      }
+    ], function (err) {
+      if (err) { throw err }
+      pull(
+        pl.read(db2),
+        pull.collect(function (err, arr) {
+          t.error(err)
+          t.deepEqual(sha1hex(stringify(arr)), '83330ec925444693f1d34026cb00ed86b7652223')
+          t.end()
+        })
+      )
+    })
   })
 
   test('sequential', function (t) {
